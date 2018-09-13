fd1367e4d390ffadc9feaa24441fce9623df151d
long.le <xuanlongvts@gmail.com>
2018-09-13 22:21:53 +0700

    branch test 1

--- maketting/asset/js/main.js
+++
@@ -1,8 +0,0 @@
-$(document).ready(function() {
-    $('.listItem .question').click(function() {
-        $('.listItem .question').removeClass('curr');
-        $(this).addClass('curr');
-
-        $('body, html').animate({ scrollTop: $(this).offset().top - 80 }, 300);
-    });
-});
